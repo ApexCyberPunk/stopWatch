fdc5ff199287c0d52d54a576c319234be6010ae0
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-27 02:55:46 -0700

    update

--- swatch/src/App.js
+++ swatch/src/App.js
@@ -1,5 +1,21 @@
+import React from 'react'
+import {useTimer} from 'use-timer'
 
 function App() {
+
+    const {time, start, pause, reset, status} = useTimer();
+
+    return (
+        <>
+        <div>
+            <button onClick={start}>Start</button>
+            <button onClick={pause}>Pause</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+           <p>Start Timer {time}</p>
+           {status === 'RUNNING' && <p> RUNNING...</p>}
+        </>
+    )
 
 }
 
